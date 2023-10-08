c00b456c0ae8034bb590799cbceb2e1a81320704
TuanTNT19 <tuantnt08@gmail.com>
2023-10-08 11:16:23 +0700

    Push Task operator

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,92 +1,77 @@
 #include "stdio.h"
 #include "main.h"
 #include "cmsis_os.h"
-TaskHandle_t NormalHandle;
+
+osThreadId defaultTaskHandle;
 TaskHandle_t AboveNormalHandle;
-SemaphoreHandle_t Sema1;
-void NormalTask(void *para1);
-void AboveNormalTask(void *para2);
-uint16_t ADC_Value;
-void Tim_Config()
+void StartDefaultTask(void const * argument);
+void AboveNormalTask (void *parameter);
+int i=0;
+	int j=0;
+int main(void)
 {
-	
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; //Enable TIMEeR2 clock
-	TIM2->ARR=65535;
-	TIM2->PSC = 7999;
-	TIM2->CR1|=(1<<0);
-	while (!(TIM2->SR & TIM_SR_UIF));
+ 
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+  xTaskCreate(AboveNormalTask, "Task01", 128, NULL, osPriorityAboveNormal+3, &AboveNormalHandle);
+
+  osKernelStart();
+
+
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
-void delay_ms(uint16_t ms)
-{
-	
-		TIM2->CNT=0;
-	while (TIM2->CNT < ms);
-}
-void GPIO_Config()
-{
-	
-	RCC->APB2ENR|=(1<<3);//enable clock for gpio b
-	GPIOB->CRL &=(0xFFFFFFF0);
-	GPIOB->CRL |=(2<<0);
-	
-}
-void ADC1_Config()
-{
-	
-	RCC->APB2ENR|=(1<<9);
-	RCC->APB2ENR|=(1<<2);//enable clock for GPIOA
-	GPIOA->CRL&=(0xFFFFFFF0);
-	GPIOA->CRL &=(0<<0);
-	ADC1->CR2|=(1<<0);
-	ADC1->CR2|=(1<<1);
-	ADC1->SQR1&=(0<<20);
-	ADC1->SQR3 &=(0<<0);
-	ADC1->CR2|=(1<<2);
-	while(!(ADC1->CR2)&(1<<2));
-	ADC1->CR2|=(1<<0);
-	ADC1->CR2|=(1<<22);
+
+
+void AboveNormalTask (void *parameter){
 	
 	
-}
-uint16_t Read_ADC1()
-{
-	while(!(ADC1->SR)&(1<<1));
-	uint16_t value = ADC1->DR;
-	return ADC1->DR;
-	
-}
-int main()
-{
-	xTaskCreate(NormalTask,"Task 1",128,NULL,0,&NormalHandle);
-	xTaskCreate(AboveNormalTask,"Task 2",128,NULL,1,&AboveNormalHandle);
-	Sema1=xSemaphoreCreateBinary();
-	GPIO_Config();
-	ADC1_Config();
-	Tim_Config();
-	osKernelStart();
 	while(1)
 	{
 		
-	
-	}
-}
-void AboveNormalTask(void *para2){
-	while(1)
-	{
-	GPIOB->ODR &=(0<<0);
-	ADC_Value = Read_ADC1();
-	if (ADC_Value>4000)
-	{
+		i++;
+		if (i==5)
+		{
+			vTaskSuspend(defaultTaskHandle);
+		}
+		if (i==10)
+		{
+			vTaskResume(defaultTaskHandle);
+			
+		}
+		vTaskDelay(500);
 		
-		xSemaphoreGive(Sema1);
 	}
-	vTaskDelay(100);
 }
+
+void StartDefaultTask(void const * argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+
+  for(;;)
+  {
+		j++;
+    osDelay(500);
+  }
+  /* USER CODE END 5 */
 }
-void NormalTask(void *para1){
-	while(1)
-	{
-	xSemaphoreTake(Sema1,portMAX_DELAY);
-	GPIOB->ODR |=(1<<0);
-	}
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM4) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
 }
