730e08147a59dbb58058d08aed2fd14e61199096
TuanTNT19 <tuantnt08@gmail.com>
2023-10-08 11:22:35 +0700

    push event group

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,59 +1,94 @@
+
+
+
 #include "stdio.h"
 #include "main.h"
 #include "cmsis_os.h"
 #include <stdlib.h>
+#include "timers.h"
 osThreadId defaultTaskHandle;
 TaskHandle_t AboveNormalHandle;
-QueueHandle_t QueuexHandle;
+//TaskHandle_t NormalTaskHandle;
+EventGroupHandle_t Group;
+EventBits_t Bit = 0;
+uint16_t a=0;
+uint16_t b=0;
+uint8_t RX_data;
+#define EVENT0    (1UL << 0)
+#define EVENT1    (1UL << 1)
+#define EVENT2    (1UL << 2)
 void StartDefaultTask(void const * argument);
 void AboveNormalTask (void *parameter);
-void USART1_Config()
+void NormalTask(void *para);
+void GPIO_config()
 {
 	RCC->APB2ENR |= (1<<2);//enable clock for gpioa
-	GPIOA->CRH&=(0xFFFFFF0F);
-	GPIOA->CRH|=(10<<4);//SET gpioa pin 9 as alternate outout function push pull for TX
-	RCC->APB2ENR|=(1<<14);//enable clock for USART1
-	USART1->BRR=833;
-	USART1->CR1|=(1<<3);//Transmit enable
-	USART1->CR1|=(1<<13);// USART enable
-}	
-void USART1_Send_char(char str)
+	// config gpioa pin 2 as alternative function TX
+	GPIOA->CRL &= (0xFFFFF0FF);
+	GPIOA-> CRL |= (10<<8);
+		// config gpioa pin 3 as alternative function RX
+	GPIOA->CRL &= (0xFFFF0FFF);
+	GPIOA->CRL |= (8<<12);//in push pull up/down
+	GPIOA->ODR |= (1<<3);// pull up
+	RCC->APB2ENR |= (1<<4); // enable clock for gpioc
+	GPIOC-> CRH &= (0xF0FFFFFF);
+	GPIOC-> CRH |= (2<<24);// set gpioc pin 14 as a led
+
+}
+void USART2_config()
 {
-	while (!(USART1->SR & USART_SR_TXE));
-	USART1->DR = str;
+	RCC->APB1ENR |= (1<<17);//enable clock for USART2
+	USART2->BRR |= 833;
+	USART2->CR1 |= (1<<13);
+	USART2->CR1 |= (1<<2); // RE=1.. Enable the Receiver
+  USART2->CR1 |= (1<<3);  // TE=1.. Enable Transmitter
 	
 }
-void USART1_Send_String(char *str)
+void USART2_Send_char(char str)
 {
-	
-	while(*str)
+	while (!(USART2->SR & USART_SR_TXE));
+	USART2->DR=str;
+}
+void USART2_Send_String(char *ch)
+{
+	while (*ch)
 	{
-		
-		USART1_Send_char(*str++);
+		 USART2_Send_char(*ch++);
 	}
 }
-struct data{
-	uint16_t k;
-	int l;
+
+uint8_t USART_Getchar(void)
+{
 	
-};
-typedef struct data test;
-uint16_t k1;
-int l1;
-uint16_t adc_value[2];
-uint16_t data_receive[2];
-uint16_t a=5;
-uint16_t b;
-uint16_t c=0;
-char str[10];
+	uint8_t Temp;
+	while (!(USART2->SR & USART_SR_RXNE));
+	Temp = USART2->DR;
+	return Temp;
+}
+void UART_Getstring(uint8_t *str, uint8_t len)
+{
+	for (uint8_t i=0;i<len;i++)
+	{
+		*str=USART_Getchar();
+				if (*str=='n')
+			{
+				*(str+1)=NULL;
+				break;
+				}
+		str++;
+  }
+}
 int main(void)
 {
-  USART1_Config();
+   GPIO_config();
+	USART2_config();
   osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
   xTaskCreate(AboveNormalTask, "Task01", 128, NULL, 3, &AboveNormalHandle);
-  QueuexHandle = xQueueCreate(2,sizeof(test));
+ // xTaskCreate(NormalTask,"Task01",128,NULL,2,&NormalTaskHandle);
+	Group =	xEventGroupCreate() ;
   osKernelStart();
+	
 
 
   while (1)
@@ -67,34 +102,66 @@ int main(void)
 
 
 void AboveNormalTask (void *parameter){
-	
-//	DMA1_Config((uint32_t)&ADC1->DR, (uint32_t)adc_value, 2);
-	test *Task_data = osPoolAlloc(QueuexHandle);
-	
-	Task_data->l=9;
+
+  USART2_Send_String("Start\n");
 	while(1)
 	{
-		
-	  Task_data->k=a;
-		xQueueSend(QueuexHandle,&Task_data ,NULL);
-		a++;
-		vTaskDelay(200);
+    Bit =  xEventGroupWaitBits(Group, EVENT0 | EVENT1 | EVENT2, pdTRUE, pdFALSE , osWaitForever);
+			if ((Bit & EVENT1) != 0)
+		{
+			USART2_Send_String("Event 1");
+		}
+		else	if ((Bit & EVENT2) != 0)
+		{
+			USART2_Send_String("Event 2");
+		}
+				else	if ((Bit & EVENT0) != 0)
+		{
+			USART2_Send_String("Event 0");
+		}
 	}
 }
+//void NormalTask(void *para)
+//{
+//	
+//	while(1)
+//	{
+//		if ((Bit & EVENT1) != 0)
+//		{
+//			USART2_Send_String("Event 1");
+//		}
+//		else	if ((Bit & EVENT2) != 0)
+//		{
+//			USART2_Send_String("Event 2");
+//		}
+//				else	if ((Bit & EVENT0) != 0)
+//		{
+//			USART2_Send_String("Event 0");
+//		}
+//	}
+//	
+//}
 
 void StartDefaultTask(void const * argument)
 {
-  /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-	test receive_data;
+   
   for(;;)
   {
-     xQueueReceive(QueuexHandle,&receive_data,osWaitForever);
-		 c++;
-//     int len1=sprintf(str,"%4d",receive_data.k);
-	    k1=receive_data.k;
-		  l1=receive_data.l;
-		  osPoolFree(QueuexHandle,&receive_data);
+		RX_data=USART_Getchar();
+    if (RX_data=='1')
+		{
+			Bit = 	xEventGroupSetBits(Group,EVENT1);
+			
+		}
+		else if (RX_data=='2')
+		{
+			
+			Bit = xEventGroupSetBits(Group,EVENT2);
+		}
+		else {
+			Bit = xEventGroupSetBits(Group,EVENT0);
+		}
+    
 		
   }
   /* USER CODE END 5 */
